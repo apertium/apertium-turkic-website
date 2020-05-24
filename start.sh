9febf75816e792acefa28d4642038e359224398a
jonorthwash <jonathan.north.washington@gmail.com>
2020-05-24 19:36:47 -0400

    oops, ports backwards

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -v /home/turkic/logs/:/home/apertium/logs/ -p 2737:8875 apertium-turkic 
+docker run -it --rm -v /home/turkic/logs/:/home/apertium/logs/ -p 8875:2737 apertium-turkic 
