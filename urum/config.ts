26b7d1025f9b98790d4dac954dc4b1c0853e2feb
Jonathan Washington <jonathan.n.washington@gmail.com>
2025-08-01 18:25:11 +0000

    adding config.ts, Debian release for Dockerfile

---
+++ urum/config.ts
@@ -0,0 +1,21 @@
+import { Config, Mode } from './src/types';
+
+export default {
+  defaultLocale: 'eng',
+  htmlUrl: 'https://beta.apertium.org/',
+  apyURL: 'https://urum.apertium.org/apy',
+  //apyURL: 'http://localhost:2737',
+
+  defaultMode: Mode.Dictionary,
+  enabledModes: new Set([Mode.Translation, Mode.Dictionary]),
+  translationChaining: true,
+
+  subtitle: 'Urum',
+  subtitleColor: 'rgb(38, 214, 220)',
+
+  stringReplacements: {
+    '{{maintainer}}': "<a href='https://wiki.apertium.org/wiki/Apertium' target='_blank' rel='noopener'>Apertium</a>",
+    '{{more_languages}}': "<a href='https://beta.apertium.org' rel='noreferrer'  target='_blank'>beta.apertium.org</a>",
+  },
+  showMoreLanguagesLink: false,
+} as Config;
